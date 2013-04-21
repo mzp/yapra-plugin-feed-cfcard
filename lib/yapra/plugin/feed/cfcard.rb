09c90b3b888428b0e951e7bc61265e2ff7304fe1
mzp <mzpppp@gmail.com>
2013-04-21 12:20:16 +0400

    bundle gem yapra-plugin-feed-cfcard

---
+++ lib/yapra/plugin/feed/cfcard.rb
@@ -0,0 +1,11 @@
+require "yapra/plugin/feed/cfcard/version"
+
+module Yapra
+  module Plugin
+    module Feed
+      module Cfcard
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/yapra/plugin/feed/cfcard/version.rb
@@ -0,0 +1,9 @@
+module Yapra
+  module Plugin
+    module Feed
+      module Cfcard
+        VERSION = "0.0.1"
+      end
+    end
+  end
+end
